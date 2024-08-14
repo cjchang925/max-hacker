42f8dd2779dbbe54e96a371cdf7f9b673cfe8a9d
Chi-Chun Chang <cjchang925@gmail.com>
2024-08-14 08:47:49 +0800

    feat: Use moment-timezone to log with Taipei time.

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,10 +1,11 @@
 import moment from "moment";
+import "moment-timezone";
 
 /**
  * 輸出時間與訊息
  * @param message 訊息
  */
 export const log = (message: string) => {
-  const now = moment().format("YYYY-MM-DD HH:mm:ss.SSS");
+  const now = moment().tz("Asia/Taipei").format("YYYY-MM-DD HH:mm:ss.SSS");
   console.log(`[${now}] INFO ${message}`);
 };

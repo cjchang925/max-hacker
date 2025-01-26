7d6782a7c70359699c388499772da6f1a522061a
Chi-Chun Chang <cjchang925@gmail.com>
2025-01-26 09:42:23 -0600

    feat: Record less order book data.

--- src/orderbook.ts
+++ src/orderbook.ts
@@ -56,41 +56,21 @@ interface OrderBookRecord {
   bid2: number;
   bid3: number;
   bid4: number;
-  bid5: number;
-  bid6: number;
-  bid7: number;
-  bid8: number;
-  bid9: number;
   bidSize0: number;
   bidSize1: number;
   bidSize2: number;
   bidSize3: number;
   bidSize4: number;
-  bidSize5: number;
-  bidSize6: number;
-  bidSize7: number;
-  bidSize8: number;
-  bidSize9: number;
   ask0: number;
   ask1: number;
   ask2: number;
   ask3: number;
   ask4: number;
-  ask5: number;
-  ask6: number;
-  ask7: number;
-  ask8: number;
-  ask9: number;
   askSize0: number;
   askSize1: number;
   askSize2: number;
   askSize3: number;
   askSize4: number;
-  askSize5: number;
-  askSize6: number;
-  askSize7: number;
-  askSize8: number;
-  askSize9: number;
   systimestamp: String;
 }
 
@@ -183,49 +163,29 @@ const main = () => {
       bid2: parseFloat(orderBook.b[2][0]),
       bid3: parseFloat(orderBook.b[3][0]),
       bid4: parseFloat(orderBook.b[4][0]),
-      bid5: parseFloat(orderBook.b[5][0]),
-      bid6: parseFloat(orderBook.b[6][0]),
-      bid7: parseFloat(orderBook.b[7][0]),
-      bid8: parseFloat(orderBook.b[8][0]),
-      bid9: parseFloat(orderBook.b[9][0]),
       bidSize0: parseFloat(orderBook.b[0][1]),
       bidSize1: parseFloat(orderBook.b[1][1]),
       bidSize2: parseFloat(orderBook.b[2][1]),
       bidSize3: parseFloat(orderBook.b[3][1]),
       bidSize4: parseFloat(orderBook.b[4][1]),
-      bidSize5: parseFloat(orderBook.b[5][1]),
-      bidSize6: parseFloat(orderBook.b[6][1]),
-      bidSize7: parseFloat(orderBook.b[7][1]),
-      bidSize8: parseFloat(orderBook.b[8][1]),
-      bidSize9: parseFloat(orderBook.b[9][1]),
       ask0: parseFloat(orderBook.a[0][0]),
       ask1: parseFloat(orderBook.a[1][0]),
       ask2: parseFloat(orderBook.a[2][0]),
       ask3: parseFloat(orderBook.a[3][0]),
       ask4: parseFloat(orderBook.a[4][0]),
-      ask5: parseFloat(orderBook.a[5][0]),
-      ask6: parseFloat(orderBook.a[6][0]),
-      ask7: parseFloat(orderBook.a[7][0]),
-      ask8: parseFloat(orderBook.a[8][0]),
-      ask9: parseFloat(orderBook.a[9][0]),
       askSize0: parseFloat(orderBook.a[0][1]),
       askSize1: parseFloat(orderBook.a[1][1]),
       askSize2: parseFloat(orderBook.a[2][1]),
       askSize3: parseFloat(orderBook.a[3][1]),
       askSize4: parseFloat(orderBook.a[4][1]),
-      askSize5: parseFloat(orderBook.a[5][1]),
-      askSize6: parseFloat(orderBook.a[6][1]),
-      askSize7: parseFloat(orderBook.a[7][1]),
-      askSize8: parseFloat(orderBook.a[8][1]),
-      askSize9: parseFloat(orderBook.a[9][1]),
       systimestamp: new Date().toISOString(),
     };
 
     records.push(record);
 
 
-    // Write to parquet every 30 minutes
-    if (records.length > 10000) {
+    // Write to parquet every 1000 rows
+    if (records.length > 1000) {
       // Write to parquet
       const schema = new parquet.ParquetSchema({
         time: { type: "UTF8" },
@@ -237,41 +197,21 @@ const main = () => {
         bid2: { type: "DOUBLE" },
         bid3: { type: "DOUBLE" },
         bid4: { type: "DOUBLE" },
-        bid5: { type: "DOUBLE" },
-        bid6: { type: "DOUBLE" },
-        bid7: { type: "DOUBLE" },
-        bid8: { type: "DOUBLE" },
-        bid9: { type: "DOUBLE" },
         bidSize0: { type: "DOUBLE" },
         bidSize1: { type: "DOUBLE" },
         bidSize2: { type: "DOUBLE" },
         bidSize3: { type: "DOUBLE" },
         bidSize4: { type: "DOUBLE" },
-        bidSize5: { type: "DOUBLE" },
-        bidSize6: { type: "DOUBLE" },
-        bidSize7: { type: "DOUBLE" },
-        bidSize8: { type: "DOUBLE" },
-        bidSize9: { type: "DOUBLE" },
         ask0: { type: "DOUBLE" },
         ask1: { type: "DOUBLE" },
         ask2: { type: "DOUBLE" },
         ask3: { type: "DOUBLE" },
         ask4: { type: "DOUBLE" },
-        ask5: { type: "DOUBLE" },
-        ask6: { type: "DOUBLE" },
-        ask7: { type: "DOUBLE" },
-        ask8: { type: "DOUBLE" },
-        ask9: { type: "DOUBLE" },
         askSize0: { type: "DOUBLE" },
         askSize1: { type: "DOUBLE" },
         askSize2: { type: "DOUBLE" },
         askSize3: { type: "DOUBLE" },
         askSize4: { type: "DOUBLE" },
-        askSize5: { type: "DOUBLE" },
-        askSize6: { type: "DOUBLE" },
-        askSize7: { type: "DOUBLE" },
-        askSize8: { type: "DOUBLE" },
-        askSize9: { type: "DOUBLE" },
         systimestamp: { type: "UTF8" },
       });
 
@@ -293,7 +233,7 @@ const main = () => {
 
       await writer.close();
       records.length = 0;
-      console.log(`${time}: Wrote 10000 records to parquet`);
+      console.log(`${time}: Wrote 1000 records to parquet`);
     }
   });
 };
